b9bd913f5d658741fda6d003240752821dbaa95c
Timur Medvedev <47181197+PSYCHXLAUGH@users.noreply.github.com>
2024-10-11 19:25:42 +0300

    Update delete_chat.py

--- delete_chat.py
+++ delete_chat.py
@@ -7,48 +7,67 @@ from datetime import datetime
 import argparse
 
 
-parser = argparse.ArgumentParser(usage=' python3 delete_chat.py --id {channel id} --secret {channel secret_key } --amojo {account amojo_id}')
-parser.add_argument('--id', help='channel id')
-parser.add_argument('--secret', help='channel secret key')
-parser.add_argument('--amojo', help='account amojo_id')
-parser.add_argument('-v', '--verbose', action='store_true')
-args = parser.parse_args()
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        usage='python3 delete_chat.py --id {channel id} --secret {channel secret_key} --amojo {account amojo_id}'
+    )
+    parser.add_argument('--id', required=True, help='Channel ID')
+    parser.add_argument('--secret', required=True, help='Channel secret key')
+    parser.add_argument('--amojo', required=True, help='Account amojo ID')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    return parser.parse_args()
 
-path = f'/v2/origin/custom/{args.id}/disconnect'
-url = f'https://amojo.amocrm.ru{path}'
-logger.debug(f'[+] url: {url}')
-payload = {'account_id': args.amojo}
-logger.debug(f'[+] payload: {payload}')
-payload = json.dumps(payload)
-checkSum = hashlib.md5(payload.encode()).hexdigest()
-method: str = 'DELETE'
-contentType = 'application/json'
-date: str = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
 
-sign = "\n".join([
-    method,
-    checkSum,
-    contentType,
-    date,
-    path
-])
+def create_signature(method, payload, path, secret):
+    check_sum = hashlib.md5(payload.encode()).hexdigest()
+    date = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
+    
+    sign_string = "\n".join([method, check_sum, 'application/json', date, path])
+    signature = hmac.new(secret.encode(), sign_string.encode(), hashlib.sha1).hexdigest()
+    
+    return check_sum, date, signature
 
-signature = hmac.new(args.secret.encode(), sign.encode(), hashlib.sha1).hexdigest()
-logger.debug(signature)
-headers = {
-    'Date': date,
-    'Content-Type': contentType,
-    'Content-MD5': checkSum,
-    'X-Signature': signature
-}
 
-logger.debug(f'headers: {headers}')
+def make_request(url, headers, payload):
+    try:
+        response = requests.delete(url, headers=headers, data=payload)
+        response.raise_for_status()  # Raise an error for HTTP errors
+        return response
+    except requests.exceptions.HTTPError as e:
+        logger.error(f'HTTP error: {e.response.status_code} - {e.response.text}')
+    except requests.exceptions.RequestException as e:
+        logger.error(f'Request error: {e}')
 
-logger.info('Trying to request...')
 
-request = requests.delete(url, headers=headers, data=payload)
+def main():
+    args = parse_arguments()
+    
+    path = f'/v2/origin/custom/{args.id}/disconnect'
+    url = f'https://amojo.amocrm.ru{path}'
+    
+    logger.debug(f'[+] URL: {url}')
+    
+    payload = json.dumps({'account_id': args.amojo})
+    logger.debug(f'[+] Payload: {payload}')
+    
+    method = 'DELETE'
+    check_sum, date, signature = create_signature(method, payload, path, args.secret)
+    
+    headers = {
+        'Date': date,
+        'Content-Type': 'application/json',
+        'Content-MD5': check_sum,
+        'X-Signature': signature
+    }
+    
+    logger.debug(f'Headers: {headers}')
+    
+    logger.info('Trying to request...')
+    response = make_request(url, headers, payload)
+    
+    if response:
+        logger.success(f'Success: {response.status_code}')
 
-if request.status_code == 200:
-    logger.success(request.status_code)
-else:
-    logger.error(f'error: {request.status_code, request.content}')
+
+if __name__ == "__main__":
+    main()
